a0472cc1fc4ef7a77695c1e342d68a7de8e22edf
dunicn <nemanjazdunic@gmail.com>
2021-02-11 14:10:16 +0100

    onSearchChange method added

--- src/App.js
+++ src/App.js
@@ -18,17 +18,19 @@ class App extends Component {
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
   }
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value })
+  }
   render() {
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, title} = this.state;
     const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>Monster Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange={e =>
-            this.setState({searchField: e.target.value})}
+          handleChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters}/>
       </div>
